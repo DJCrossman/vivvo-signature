da168977f1dcf6ef5805e566943341bd4b62ff4a
djcrossman <david@crossman.io>
2017-07-20 10:48:11 -0600

    Added clipboard tool

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 export class App {
+  signature;
   name = 'Test McTesterson';
   position = 'Software Developer';
   img = 'http://imgur.com/N0JqEb7.png';
@@ -9,5 +10,15 @@ export class App {
   instagram = '';
   get formattedPhone() {
     return this.phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+  }
+  copyToClipboard() {
+    window.getSelection().selectAllChildren(this.signature);
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copying text command was ' + msg);
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
   }
 }
