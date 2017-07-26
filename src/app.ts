ab6f12651a88989a471f4fd866868301b95bf796
djcrossman <david@crossman.io>
2017-07-26 16:40:03 -0600

    Updated app

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,29 @@ export class App {
   name = '';
   position = '';
   img = 'http://imgur.com/N0JqEb7.png';
-  phone = '';
-  facebook = '';
-  github = '';
-  linkedin = '';
-  twitter = '';
-  instagram = '';
-  get formattedPhone() {
-    return this.phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+  _phone = '';
+  _facebook = '';
+  _github = '';
+  _linkedin = '';
+  _twitter = '';
+  _instagram = '';
+  get facebook() {
+    return this._facebook ? 'https://www.facebook.com/' + this._facebook : '';
+  }
+  get github() {
+    return this._github ? 'https://github.com/' + this._github : '';
+  }
+  get linkedin() {
+    return this._linkedin ? 'https://www.linkedin.com/in/' + this._linkedin : '';
+  }
+  get twitter() {
+    return this._twitter ? 'https://twitter.com/' + this._twitter : '';
+  }
+  get instagram() {
+    return this._instagram ? 'https://www.instagram.com/' + this._instagram : '';
+  }
+  get phone() {
+    return this._phone.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
   }
   copyToClipboard() {
     window.getSelection().selectAllChildren(this.signature);
