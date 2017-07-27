f9deb21e018d5791f8980bf2a48bdde86344a4cd
djcrossman <david@crossman.io>
2017-07-27 08:35:58 -0600

    removed line and added more link support

--- src/app.ts
+++ src/app.ts
@@ -10,18 +10,23 @@ export class App {
   _twitter = '';
   _instagram = '';
   get facebook() {
+    this._facebook = this._facebook.replace('https://www.facebook.com/', '');
     return this._facebook ? 'https://www.facebook.com/' + this._facebook : '';
   }
   get github() {
+    this._github = this._github.replace('https://github.com/', '');
     return this._github ? 'https://github.com/' + this._github : '';
   }
   get linkedin() {
+    this._linkedin = this._linkedin.replace('https://www.linkedin.com/in/', '');
     return this._linkedin ? 'https://www.linkedin.com/in/' + this._linkedin : '';
   }
   get twitter() {
+    this._twitter = this._twitter.replace('https://twitter.com/', '');
     return this._twitter ? 'https://twitter.com/' + this._twitter : '';
   }
   get instagram() {
+    this._instagram = this._instagram.replace('https://www.instagram.com/', '');
     return this._instagram ? 'https://www.instagram.com/' + this._instagram : '';
   }
   get phone() {
